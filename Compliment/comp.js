32839e7de32c8a6b70d979e89a09213a2f069a6a
Shea <sheaslade@gmail.com>
2020-07-18 15:58:02 -0600

    small fixes to css

--- Compliment/comp.js
+++ Compliment/comp.js
@@ -5,15 +5,15 @@ script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
 var compliments = [
-    'You look nice today',
-    'You are loved',
+    'You look nice today.',
+    'You are loved.',
     'Have you been working out?',
     'I feel the need to impress you.',
     'You could probably lead a rebellion.',
-    'My mom always asks me why I can\'t be more like you',
+    'My mom always asks me why I can\'t be more like you.',
     'Glorious things are waiting for you.',
-    'Your eyes are disarming',
-    'You have a great sense of humor',
+    'Your eyes are disarming.',
+    'You have a great sense of humor.',
     'You\'re that “Nothing” when people ask me what I\'m thinking about.',
     'I bet you make babies smile.',
     'On a scale from 1 to 10, you’re an 11.',
@@ -21,18 +21,22 @@ var compliments = [
     'Somehow you make time stop and fly at the same time.',
     'Is that your picture next to "charming" in the dictionary?',
     'You\'re great at figuring stuff out.',
-    'You are a light shining in darkness',
-    'I can\'t take my eys off you',
-    'You are stronger than you think',
-    'Anyone would be lucky to have you',
+    'You are a light shining in darkness.',
+    'I can\'t take my eys off you.',
+    'You are stronger than you think.',
+    'Anyone would be lucky to have you.',
     'You have a great smile.',
+    'I have heard so many good things about you.',
+    'I love the way you laugh',
+    'The stars look brighter with you under them',
 ]
 
 var colors = ['#16a085', '#27ae60', '#2c3e50', 
 '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', 
 "#472E32", "#BDBB99", "#77B1A9", "#73A857", "#2ab7ca", 
 '#f4b6c2', '#005b96', '#851e3e', '#dec3c3', '#f6cd61',
-'#feb2a8', '#ee4035'];
+'#feb2a8', '#ee4035', '#ff66cc', '#ffccee', '#ff8c66',
+'#668cff'];
 
 //generate random number[1-19]
 function rand(len){
@@ -54,12 +58,12 @@ function color(){
 }
 
 // document.onload = generate(); //load first compliment
-document.onload = color(); // load first color
+// document.onload = color(); // load first color
 
 /*Grabs new quote on button press */
 document.getElementById("new-comp").addEventListener('click', function(){
     $("#compliment").fadeOut(1);
-    color();
-    generate();
-    $("#compliment").fadeIn(2500);
+    color(); // new color
+    generate(); //new quote
+    $("#compliment").fadeIn(2500); //fading for text
 });

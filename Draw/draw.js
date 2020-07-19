e8dc8a1e55c638c3a549c3840cca356631b8cc21
Shea <sheaslade@gmail.com>
2020-07-18 18:54:18 -0600

    removed change

--- Draw/draw.js
+++ Draw/draw.js
@@ -52,51 +52,6 @@ canvas.addEventListener('mouseup', (e) => {
   }
 })
 
-canvas.addEventListener("touchstart", function(e) {
-  mousePos = getTouchPos(canvas, e);
-  startX = mousePos.x;
-  startY = mousePos.y;
-  var touch = e.touches[0];
-  var mouseEvent = new MouseEvent("mousedown", {
-    clientX: touch.clientX,
-    clientY: touch.clientY
-  });
-  canvas.dispatchEvent(mouseEvent);
-}, false);
-
-canvas.addEventListener("touchend", function(e) {
-  positions.push({
-    s_x: startX,
-    s_y: startY,
-    e_x: endX,
-    e_y: endY
-  });
-  drawAllControls();
-  console.log(positions);
-  var mouseEvent = new MouseEvent("mouseup", {});
-  canvas.dispatchEvent(mouseEvent);
-}, false);
-
-canvas.addEventListener("touchmove", function(e) {
-  var touch = e.touches[0];
-  endX = touch.clientX;
-  endY = touch.clientY;
-  drawSquare()
-  var mouseEvent = new MouseEvent("mousemove", {
-    clientX: touch.clientX,
-    clientY: touch.clientY
-  });
-  canvas.dispatchEvent(mouseEvent);
-}, false);
-
-function getTouchPos(canvasDom, touchEvent) {
-  var rect = canvasDom.getBoundingClientRect();
-  return {
-    x: touchEvent.touches[0].pageX - rect.left,
-    y: touchEvent.touches[0].pageY - rect.top
-  };
-}
-
 
 function draw(context,x1,y1,x2,y2){
   ctx.strokeStyle = selecting(); //choose color
